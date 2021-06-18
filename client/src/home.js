9d124e46039923d16b328270c0644dd4fe486aa7
DTUSHAR <tusharsahu3627@gmail.com>
2021-06-18 21:56:37 +0530

    change

--- client/src/home.js
+++ client/src/home.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import logo from './logo.jpg';
 
 export default function home() {
-    
     return (
-         <div >
-            {/* <marquee behavior="scroll" direction="right" scrollamount="20" className="header1">Welcome to Mini Bank</marquee> */}
-            <h3 className="header1">Welcome to Mini Bank</h3>
-            <img className="center" src="logo.jpg" alt="Bank Image"/>
-         </div>
+         <>
+         <marquee behavior="scroll" direction="right" scrollamount="15" className="details">Contact NO:93737546XX , Email: Tushar@gmail.com</marquee>
+         <h3 className="header1">Welcome to mini Bank</h3>
+         <img className="center-image" src={logo}/>
+         <footer class="page-footer font-small blue">
+
+             <div class="footer-copyright text-center py-3">© 2021 Copyright:
+              <a href="https://limitless-bastion-83164.herokuapp.com/">Mini Bank Sparks pvt ltd</a>
+             </div>
+
+        </footer>
+         </>
     )
 }
